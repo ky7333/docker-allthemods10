9a7052f6d7e9a7d8472514d34623ed2ec42ddb50
Kyle Smith <80354573+ky7333@users.noreply.github.com>
2026-02-14 01:45:41 +0000

    Fix download link

--- launch.sh
+++ launch.sh
@@ -17,7 +17,7 @@ fi
 if ! [[ -f "Server-Files-$SERVER_VERSION.zip" ]]; then
     rm -fr config defaultconfigs kubejs mods packmenu Server-Files-* neoforge*
 
-    curl -Lo "Server-Files-$SERVER_VERSION.zip" "https://mediafilez.forgecdn.net/files/7361/524/ServerFiles-$SERVER_VERSION.zip" || exit 9
+    curl -Lo "Server-Files-$SERVER_VERSION.zip" "https://mediafilez.forgecdn.net/files/7558/613/ServerFiles-5.5.zip" || exit 9
    
     unzip -u -o "Server-Files-$SERVER_VERSION.zip" -d /data
     DIR_TEST="ServerFiles-$SERVER_VERSION"
